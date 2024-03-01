f5a22d19da4871fb9f2e709e96ecc31cc1762dbc
jonatan <jonatan5524@gmail.com>
2024-03-01 22:31:00 +0200

    change keybindings zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -31,9 +31,17 @@ setopt HIST_REDUCE_BLANKS
 #   like: git comm-[tab]
 setopt complete_aliases
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^?' backward-delete-char
+# To see the key combo you want to use just do:
+# cat > /dev/null
+# And press it
+
+bindkey "^K"      kill-whole-line                      # ctrl-k
+bindkey "^R"      history-incremental-search-backward  # ctrl-r
+bindkey "^A"      beginning-of-line                    # ctrl-a  
+bindkey "^E"      end-of-line                          # ctrl-e
+bindkey "[B"      history-search-forward               # down arrow
+bindkey "[A"      history-search-backward              # up arrow
+bindkey "^D"      delete-char                          # ctrl-d
+bindkey "^F"      forward-char                         # ctrl-f
+bindkey "^B"      backward-char                        # ctrl-b
+bindkey -e   # Default to standard emacs bindings, regardless of editor string
