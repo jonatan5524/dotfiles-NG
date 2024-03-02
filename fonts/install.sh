ccfaeb1bdc6d894040a22a06d938c2f987e27034
jonatan <jonatan5524@gmail.com>
2024-03-02 12:59:35 +0200

    add auto font install

---
+++ fonts/install.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+#
+# Script that runs in bin/dot and install all the fonts needed
+
+echo "Installing fonts..."
+
+font_names=("FiraCode" "IosevkaTerm")
+
+for font_name in "${font_names[@]}"; do
+	echo "install font $font_name..."
+
+	curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
+	mkdir -p  "$HOME/.fonts"
+	unzip "$font_name.zip" -d "$HOME/.fonts/$font_name/"
+	fc-cache -fv
+	rm -rf "$font_name.zip"
+	
+	echo "done!"     
+done
